d5fe9e9f381fb1a19dc2de791d74813ee668d222
Alena Batitskaya <solar.rust@gmail.com>
2018-01-29 16:02:57 +0300

    📸 Подключен плагин оптимизации картинок

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,19 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    imagemin: {
+      dynamic: {
+        files: [{
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true,
+          expand: true,
+          cwd: 'images/src/',
+          src: ['*.{png,jpg,gif}'],
+          dest: 'images/dist/'
+        }]
+      }
+    },
+
     postcss: {
       options: {
         map: true,
@@ -18,20 +32,27 @@ module.exports = function(grunt) {
 
     watch: {
 			css: {
-			    files: [
-            'styles/src/*.css'
-          ],
-			    tasks: ['postcss', 'watch'],
-			    options: {
-              livereload: true,
-			        spawn: false,
-			        event: ['all'],
-			    }
+		    files: [
+          'styles/src/*.css'
+        ],
+		    tasks: ['postcss', 'watch'],
+		    options: {
+            livereload: true,
+		        spawn: false,
+		        event: ['all'],
+		    }
+      },
+      imgs: {
+        files: [
+          'images/src/*.css'
+        ],
+		    tasks: ['imagemin', 'watch'],
 			}
     }
   });
   grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['postcss', 'watch']);
+  grunt.registerTask('default', ['imagemin', 'postcss', 'watch']);
 };
